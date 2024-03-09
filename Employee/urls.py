31f3fd1443fa27d03e0e3d50a3351434c59e4178
anvesh5222 <anveshchary5222@gmail.com>
2024-03-09 17:27:34 +0530

    first commit

--- Employee/urls.py
+++ Employee/urls.py
@@ -22,7 +22,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',views.home,name='home'),
+    path('',views.home,name='home'),
     path('employees/',include('salary.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
